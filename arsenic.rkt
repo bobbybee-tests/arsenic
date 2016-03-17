0557c38f9e8b3e88839f2a500e33f7b46ba3f3a9
Alyssa <alyssa@backtick.town>
2016-03-17 18:20:57 -0400

    Proper Racket :-)

--- arsenic.rkt
+++ arsenic.rkt
@@ -9,15 +9,24 @@
 (require racket)
 (require json)
 
-(define source-path
-  (vector-ref
-    (current-command-line-arguments)
-    0))
+; Unzipped sb2 directory, from command-line or working directory
+
+(define source-directory
+  (let ([arguments (current-command-line-arguments)])
+    (if (= (vector-length arguments) 0)
+      (current-directory)
+      (vector-ref arguments 0))))
+
+; Parses project.json from a given directory
 
 (define (source-project path)
-  (let ([handle (open-input-file path)])
-    (let ([json (read-json handle)])
-      (close-input-port handle)
-      json)))
+  (let* ([handle (open-input-file (build-path path "project.json"))]
+         [json (read-json handle)])
+    (close-input-port handle)
+     json))
 
-(source-project source-path)
+; Project hashmap
+(define project (source-project source-directory))
+
+; Test
+(hash-ref project (string->symbol "objName"))
